9f54ed3baaf25b62ea5784e4b059a000e5543c75
JM <jmarmucki@tm1.edu.pl>
2023-12-19 09:08:06 +0100

    Update: add conversion from raw csv to Table model for loaded data

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
+from models import Table
+from utils import get_student_with_best_average_score
+
 DATA_FILES_BASE_PATH = './data'
 
 def load_data():
@@ -8,3 +11,9 @@ def load_data():
   with open(f'{DATA_FILES_BASE_PATH}/przedmioty.txt', 'r') as file:
     classes_data = file.read()
   return (students_data, grades_data, classes_data)
+
+students_raw_data, grades_raw_data, classes_raw_data = load_data()
+
+students_table = Table(students_raw_data)
+grades_table = Table(grades_raw_data)
+classes_table = Table(classes_raw_data)
